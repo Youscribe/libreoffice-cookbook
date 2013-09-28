c297e6c4999377f417e1c8242d7abc7b9c496d12
Guilhem Lettron <guilhem@lettron.fr>
2013-09-28 11:07:04 +0200

    Add support for windows Use chocolatey for this

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,10 @@
 default['libreoffice']['version']        = "4.0"
 default['libreoffice']['repo']           = false
-default['libreoffice']['install_method'] = "package"
 default['libreoffice']['upgrade']        = false
+
+case node['platform_family']
+when "windows"
+  default['libreoffice']['install_method'] = "chocolatey"
+else
+  default['libreoffice']['install_method'] = "package"
+end
--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,9 @@ license          "Apache 2.0"
 description      "Install libreoffice"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
+
 depends          "apt"
+depends          "chocolatey"
+
 supports         "ubuntu"
+supports         "windows"
